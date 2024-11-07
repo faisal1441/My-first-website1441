87ec3a4264d8f7febd3adca8c385fd375547a472
faisal1441 <160399431+faisal1441@users.noreply.github.com>
2024-11-07 08:48:59 +0500

    Add files via upload

--- loading.tsx
+++ loading.tsx
@@ -2,6 +2,6 @@ import React from "react";
 <script src="https://cdn.tailwindcss.com"></script>
 export default function Loading(){
     return (
-        <h1 className="bg-cyan-500 from-neutral-400"> Loading page of Services</h1>
+        <h1 className="bg-purple-400 font-thin"> Loading page of Program</h1>
     )
 } 
--- navigation.tsx
+++ navigation.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Link from "next/link";
-<script src="https://cdn.tailwindcss.com"></script>
-export default function Navbar(){
-    return( 
-        <fieldset>
-        <div className="font-bold bg-emerald-200">
-            <Link  href ="/About">About</Link>
-            <br />
-              
-            <Link href ="/Contact">Contact</Link>
-            <br />
-            <Link href ="/Program">Program</Link>
-            <br />
-            <Link href ="/Services">Services</Link>
-            <br />
-            <Link href ="/Car">Cars</Link>
-        </div>
-        </fieldset>
-    )
+
+export default function Navbar() {
+  return (
+    <header className="absolute top-10 w-full">
+      <nav className="p-6 bg-yellow-400 text-white h-2 flex flex-col md:flex-row md:justify-start
+       md:items-center font-bold space-y-2 md:space-y-0 md:space-x-4 m-0">
+        <Link href="/About">About</Link>
+        <Link href="/Contact">Contact</Link>
+        <Link href="/Program">Program</Link>
+        <Link href="/Services">Services</Link>
+        <Link href="/Car">Cars</Link>
+      </nav>
+    </header>
+  );
 }
